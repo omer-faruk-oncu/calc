f5800106bc2d7f5a554e9890a94f6bdfedba41b3
Omer <farukoncu78@gmail.com>
2024-01-24 17:37:45 +0300

    first commit

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 let num1 = Number(prompt("Enter first number : "))
 let op = prompt("Choose an operation +, -, *, / ")
-let num2 = Number(prompt(("Enter second number : ")))
+let num2 = Number(prompt("Enter second number : "))
 let result
 if (op == "+"){
     let result = num1 + num2
